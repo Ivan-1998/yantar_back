e582212dda89b9cb49b475cde2852f4aeb080eaa
tigfamon <tigfamon@gmail.com>
2019-11-07 23:26:39 +0700

    # remove console.log

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -94,7 +94,6 @@ exports.uploadPhotoProduct = asyncHandler(async (req, res, next) => {
   file.name = `photo_product_${product._id}${path.parse(file.name).ext}`;
   file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async err => {
     if (err) {
-      console.error(err);
       return next(new ErrorResponse('Problem with file upload', 500));
     }
 
